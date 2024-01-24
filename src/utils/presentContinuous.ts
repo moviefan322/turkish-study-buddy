9deb17499978267bb0d69202f44f8f99853fed46
moviefan322 <philman202@yahoo.com>
2024-01-23 22:27:53 -0500

    starts turkish present continuous conjugation

--- src/utils/presentContinuous.ts
+++ src/utils/presentContinuous.ts
@@ -11,6 +11,12 @@ const pronounVerbPairs = [
   ["You (formal)", "are"],
 ];
 
+const turkishPronouns = ["Ben", "Sen", "O", "Biz", "Siz", "Onlar"];
+
+const randomVerb = verbs[Math.floor(Math.random() * verbs.length)];
+const randomPronounVerbPair =
+  pronounVerbPairs[Math.floor(Math.random() * pronounVerbPairs.length)];
+
 const consonants = [
   "b",
   "c",
@@ -36,6 +42,32 @@ const consonants = [
 
 const vowels = ["a", "e", "i", "o", "u"];
 
+const turkishConsonants = [
+  "b",
+  "c",
+  "ç",
+  "d",
+  "f",
+  "g",
+  "ğ",
+  "h",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "p",
+  "r",
+  "s",
+  "ş",
+  "t",
+  "v",
+  "y",
+  "z",
+];
+
+const turkishVowels = ["a", "e", "ı", "i", "o", "ö", "u", "ü"];
+
 export function presentContinuous(verb: string) {
   const lastLetter = verb[verb.length - 1];
   const secondToLastLetter = verb[verb.length - 2];
@@ -60,7 +92,7 @@ export function presentContinuous(verb: string) {
 }
 
 export function presentContinuousSentence(
-  pronounVerbPair: [string, string],
+  pronounVerbPair: string[],
   verb: string
 ) {
   return `${pronounVerbPair[0]} ${pronounVerbPair[1]} ${presentContinuous(
@@ -68,5 +100,14 @@ export function presentContinuousSentence(
   )}.`;
 }
 
-const randomVerb = verbs[Math.floor(Math.random() * verbs.length)];
-console.log(presentContinuous(randomVerb.english));
+export const conjugateTurkishVerb = (verb: string, pronoun: string) => {
+  // separate stem from ending
+  let verbStem = verb.slice(0, -2); // remove -mak or -mek
+  let vowelHarmony = "";
+  // remove consonants from stem
+  const vowelsInStem = verbStem.split("").filter((letter) => {
+    return turkishVowels.includes(letter);
+  })
+
+  switch (vowelsInStem) {}
+};
