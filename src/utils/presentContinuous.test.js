106139635c80689fead8e43c6ed0599c8f63301c
moviefan322 <philman202@yahoo.com>
2024-01-23 23:56:06 -0500

    present continuous function works perfectly

--- src/utils/presentContinuous.test.js
+++ src/utils/presentContinuous.test.js
@@ -32,11 +32,6 @@ describe("presentContinuous", () => {
     expect(presentContinuous("picnic")).toBe("picnicking");
   });
 
-  // test('Does not double the letter if the word has two or more syllables and the last part is not stressed"', () => {
-  //   expect(presentContinuous("benefit")).toBe("benefiting");
-  //   expect(presentContinuous("visit")).toBe("visiting");
-  // });
-
   test("handles other verbs correctly", () => {
     expect(presentContinuous("fuck")).toBe("fucking");
   });
@@ -56,7 +51,7 @@ describe("conjugateTurkishVerb", () => {
 });
 
 describe("conjugateTurkishVerb", () => {
-  test("conjugates properly in 1st person", () => {
+  test("conjugates properly for ben", () => {
     expect(conjugateTurkishVerb("yapmak", "Ben")).toBe("yapıyorum");
     expect(conjugateTurkishVerb("çalışmak", "Ben")).toBe("çalışıyorum");
     expect(conjugateTurkishVerb("vermek", "Ben")).toBe("veriyorum");
@@ -71,5 +66,90 @@ describe("conjugateTurkishVerb", () => {
     expect(conjugateTurkishVerb("etmek", "Ben")).toBe("ediyorum");
     expect(conjugateTurkishVerb("tatmek", "Ben")).toBe("tadıyorum");
     expect(conjugateTurkishVerb("seyretmek", "Ben")).toBe("seyrediyorum");
+  });
+
+  test("conjugates properly for sen", () => {
+    expect(conjugateTurkishVerb("yapmak", "Sen")).toBe("yapıyorsun");
+    expect(conjugateTurkishVerb("çalışmak", "Sen")).toBe("çalışıyorsun");
+    expect(conjugateTurkishVerb("vermek", "Sen")).toBe("veriyorsun");
+    expect(conjugateTurkishVerb("bilmek", "Sen")).toBe("biliyorsun");
+    expect(conjugateTurkishVerb("düşünmek", "Sen")).toBe("düşünüyorsun");
+    expect(conjugateTurkishVerb("görmek", "Sen")).toBe("görüyorsun");
+    expect(conjugateTurkishVerb("istemek", "Sen")).toBe("istiyorsun");
+    expect(conjugateTurkishVerb("ödemek", "Sen")).toBe("ödüyorsun");
+    expect(conjugateTurkishVerb("yemek", "Sen")).toBe("yiyorsun");
+    expect(conjugateTurkishVerb("demek", "Sen")).toBe("diyorsun");
+    expect(conjugateTurkishVerb("gitmek", "Sen")).toBe("gidiyorsun");
+    expect(conjugateTurkishVerb("etmek", "Sen")).toBe("ediyorsun");
+    expect(conjugateTurkishVerb("tatmek", "Sen")).toBe("tadıyorsun");
+    expect(conjugateTurkishVerb("seyretmek", "Sen")).toBe("seyrediyorsun");
+  });
+
+  test("conjugates properly for O", () => {
+    expect(conjugateTurkishVerb("yapmak", "O")).toBe("yapıyor");
+    expect(conjugateTurkishVerb("çalışmak", "O")).toBe("çalışıyor");
+    expect(conjugateTurkishVerb("vermek", "O")).toBe("veriyor");
+    expect(conjugateTurkishVerb("bilmek", "O")).toBe("biliyor");
+    expect(conjugateTurkishVerb("düşünmek", "O")).toBe("düşünüyor");
+    expect(conjugateTurkishVerb("görmek", "O")).toBe("görüyor");
+    expect(conjugateTurkishVerb("istemek", "O")).toBe("istiyor");
+    expect(conjugateTurkishVerb("ödemek", "O")).toBe("ödüyor");
+    expect(conjugateTurkishVerb("yemek", "O")).toBe("yiyor");
+    expect(conjugateTurkishVerb("demek", "O")).toBe("diyor");
+    expect(conjugateTurkishVerb("gitmek", "O")).toBe("gidiyor");
+    expect(conjugateTurkishVerb("etmek", "O")).toBe("ediyor");
+    expect(conjugateTurkishVerb("tatmek", "O")).toBe("tadıyor");
+    expect(conjugateTurkishVerb("seyretmek", "O")).toBe("seyrediyor");
+  });
+
+  test("conjugates properly for Biz", () => {
+    expect(conjugateTurkishVerb("yapmak", "Biz")).toBe("yapıyoruz");
+    expect(conjugateTurkishVerb("çalışmak", "Biz")).toBe("çalışıyoruz");
+    expect(conjugateTurkishVerb("vermek", "Biz")).toBe("veriyoruz");
+    expect(conjugateTurkishVerb("bilmek", "Biz")).toBe("biliyoruz");
+    expect(conjugateTurkishVerb("düşünmek", "Biz")).toBe("düşünüyoruz");
+    expect(conjugateTurkishVerb("görmek", "Biz")).toBe("görüyoruz");
+    expect(conjugateTurkishVerb("istemek", "Biz")).toBe("istiyoruz");
+    expect(conjugateTurkishVerb("ödemek", "Biz")).toBe("ödüyoruz");
+    expect(conjugateTurkishVerb("yemek", "Biz")).toBe("yiyoruz");
+    expect(conjugateTurkishVerb("demek", "Biz")).toBe("diyoruz");
+    expect(conjugateTurkishVerb("gitmek", "Biz")).toBe("gidiyoruz");
+    expect(conjugateTurkishVerb("etmek", "Biz")).toBe("ediyoruz");
+    expect(conjugateTurkishVerb("tatmek", "Biz")).toBe("tadıyoruz");
+    expect(conjugateTurkishVerb("seyretmek", "Biz")).toBe("seyrediyoruz");
+  });
+
+  test("conjugates properly for Siz", () => {
+    expect(conjugateTurkishVerb("yapmak", "Siz")).toBe("yapıyorsunuz");
+    expect(conjugateTurkishVerb("çalışmak", "Siz")).toBe("çalışıyorsunuz");
+    expect(conjugateTurkishVerb("vermek", "Siz")).toBe("veriyorsunuz");
+    expect(conjugateTurkishVerb("bilmek", "Siz")).toBe("biliyorsunuz");
+    expect(conjugateTurkishVerb("düşünmek", "Siz")).toBe("düşünüyorsunuz");
+    expect(conjugateTurkishVerb("görmek", "Siz")).toBe("görüyorsunuz");
+    expect(conjugateTurkishVerb("istemek", "Siz")).toBe("istiyorsunuz");
+    expect(conjugateTurkishVerb("ödemek", "Siz")).toBe("ödüyorsunuz");
+    expect(conjugateTurkishVerb("yemek", "Siz")).toBe("yiyorsunuz");
+    expect(conjugateTurkishVerb("demek", "Siz")).toBe("diyorsunuz");
+    expect(conjugateTurkishVerb("gitmek", "Siz")).toBe("gidiyorsunuz");
+    expect(conjugateTurkishVerb("etmek", "Siz")).toBe("ediyorsunuz");
+    expect(conjugateTurkishVerb("tatmek", "Siz")).toBe("tadıyorsunuz");
+    expect(conjugateTurkishVerb("seyretmek", "Siz")).toBe("seyrediyorsunuz");
+  });
+
+  test("conjugates properly for Onlar", () => {
+    expect(conjugateTurkishVerb("yapmak", "Onlar")).toBe("yapıyor(lar)");
+    expect(conjugateTurkishVerb("çalışmak", "Onlar")).toBe("çalışıyor(lar)");
+    expect(conjugateTurkishVerb("vermek", "Onlar")).toBe("veriyor(lar)");
+    expect(conjugateTurkishVerb("bilmek", "Onlar")).toBe("biliyor(lar)");
+    expect(conjugateTurkishVerb("düşünmek", "Onlar")).toBe("düşünüyor(lar)");
+    expect(conjugateTurkishVerb("görmek", "Onlar")).toBe("görüyor(lar)");
+    expect(conjugateTurkishVerb("istemek", "Onlar")).toBe("istiyor(lar)");
+    expect(conjugateTurkishVerb("ödemek", "Onlar")).toBe("ödüyor(lar)");
+    expect(conjugateTurkishVerb("yemek", "Onlar")).toBe("yiyor(lar)");
+    expect(conjugateTurkishVerb("demek", "Onlar")).toBe("diyor(lar)");
+    expect(conjugateTurkishVerb("gitmek", "Onlar")).toBe("gidiyor(lar)");
+    expect(conjugateTurkishVerb("etmek", "Onlar")).toBe("ediyor(lar)");
+    expect(conjugateTurkishVerb("tatmek", "Onlar")).toBe("tadıyor(lar)");
+    expect(conjugateTurkishVerb("seyretmek", "Onlar")).toBe("seyrediyor(lar)");
   });
 });
