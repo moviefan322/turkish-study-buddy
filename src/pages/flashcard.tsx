90051ec563e160ac5b585c27139a96eeab17c408
moviefan322 <philman202@yahoo.com>
2024-01-25 15:23:56 -0500

    moves flashcard component to flashcard page

--- src/pages/flashcard.tsx
+++ src/pages/flashcard.tsx
@@ -1,11 +1,90 @@
-import Flashcards from "@/components/flashcard/Flashcards"
+import React, { useState } from "react";
+import Layout from "@/components/Layout";
+import { verbs } from "@/data/vocab/verbs";
 
-const flashcard = () => {
-  return (
-    <div>
-      <Flashcards />
-    </div>
-  )
+interface Flashcard {
+  turkish: string;
+  english: string;
 }
 
-export default flashcard
+const Flashcard = () => {
+  const [showFlashcards, setShowFlashcards] = useState(false);
+  const [sideUp, setSideUp] = useState("");
+  const [flashcards, setFlashcards] = useState([...verbs]);
+  const [randomCard, setRandomCard] = useState<Flashcard>({ turkish: "", english: "" });
+  const [showAnswer, setShowAnswer] = useState(false);
+
+  const shuffleDeck = () => {
+    let workingArray = [...flashcards];
+    for (let i = flashcards.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * i);
+      let temp = flashcards[i];
+      workingArray[i] = workingArray[j];
+      workingArray[j] = temp;
+      setFlashcards(workingArray);
+    }
+  };
+
+  const randomFlashcard = () => {
+    let randomIndex = Math.floor(Math.random() * flashcards.length);
+    return flashcards[randomIndex];
+  };
+
+  const randomizeCard = () => {
+    setRandomCard(randomFlashcard());
+  };
+
+  const handleShowFlashcards = () => {
+    shuffleDeck();
+    randomizeCard();
+    setShowFlashcards(true);
+  };
+
+  const handleReveal = () => {
+    setShowAnswer(true);
+  };
+
+  const handleNext = () => {
+    setShowAnswer(false);
+    randomizeCard();
+  };
+
+  console.log(flashcards);
+
+  return (
+    <Layout>
+      <h1>Şimşek Kartlar</h1>
+      {!showFlashcards ? (
+        <button className="btn btn-lg btn-dark text-light" onClick={() => handleShowFlashcards()}>
+          Begin
+        </button>
+      ) : (
+        <div className={`d-flex flex-column justify-content-center align-items-center w-100`}>
+          <div
+            className={`justify-self-center align-self-center fs-5 fw-bolder border border-2 border-dark p-5 w-100 text-center mb-4 bg-dark text-light`}
+          >
+            {randomCard.turkish}
+          </div>
+          <div
+            className={`justify-self-center align-self-center fs-5 fw-bolder border border-2 border-dark p-5 w-100 text-center bg-dark text-light`}
+          >
+            {showAnswer ? randomCard.english : "?"}
+          </div>
+          <div className="d-flex flex-row justify-content-around align-items-center w-100 mt-5">
+            {showAnswer ? (
+              <button className="btn btn-lg btn-info" onClick={() => handleNext()}>
+                Next
+              </button>
+            ) : (
+              <button className="btn btn-lg btn-success" onClick={() => handleReveal()}>
+                Reveal
+              </button>
+            )}
+          </div>
+        </div>
+      )}
+    </Layout>
+  );
+};
+
+export default Flashcard;
