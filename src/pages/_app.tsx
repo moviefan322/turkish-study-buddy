5430286b83eb3db472f5f4b188a322850bb8e274
moviefan322 <philman202@yahoo.com>
2024-01-26 10:08:23 -0500

    adds head with meta title favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,21 @@ import "@/styles/bootstrap.scss";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { useEffect } from "react";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
     require("bootstrap/dist/js/bootstrap.bundle.min.js");
   }, []);
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Study Turkish</title>
+        <link rel="icon" href="/favicon.svg" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
