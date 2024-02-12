565cffc4c80856b56fd6087b97bec5ef0cd9e231
moviefan322 <philman202@yahoo.com>
2024-02-12 17:53:06 -0500

    adds function and test for noun suffix nominal

--- src/utils/possessive.test.ts
+++ src/utils/possessive.test.ts
@@ -1,4 +1,4 @@
-import { possessivePronoun } from "./possessive";
+import { possessivePronoun, possessiveNounSuffix } from "./possessive";
 
 describe("possessivePronoun", () => {
   it("should return the correct possessive pronoun for 'ben'", () => {
@@ -18,5 +18,66 @@ describe("possessivePronoun", () => {
   });
   it("should return the correct possessive pronoun for 'onlar'", () => {
     expect(possessivePronoun("onlar")).toBe("onların");
+  });
+});
+
+describe("possessiveNounSuffix", () => {
+  it("should return the correct possessive noun suffix for 'ben'", () => {
+    expect(possessiveNounSuffix("ben", "baba")).toBe("babam");
+    expect(possessiveNounSuffix("ben", "anne")).toBe("annem");
+    expect(possessiveNounSuffix("ben", "manto")).toBe("mantom");
+    expect(possessiveNounSuffix("ben", "ütü")).toBe("ütüm");
+    expect(possessiveNounSuffix("ben", "anahtar")).toBe("anahtarım");
+    expect(possessiveNounSuffix("ben", "ev")).toBe("evim");
+    expect(possessiveNounSuffix("ben", "kol")).toBe("kolum");
+    expect(possessiveNounSuffix("ben", "köy")).toBe("köyüm");
+  });
+  it("should return the correct possessive noun suffix for 'sen'", () => {
+    expect(possessiveNounSuffix("sen", "baba")).toBe("baban");
+    expect(possessiveNounSuffix("sen", "anne")).toBe("annen");
+    expect(possessiveNounSuffix("sen", "ütü")).toBe("ütün");
+    expect(possessiveNounSuffix("sen", "anahtar")).toBe("anahtarın");
+    expect(possessiveNounSuffix("sen", "ev")).toBe("evin");
+    expect(possessiveNounSuffix("sen", "kol")).toBe("kolun");
+    expect(possessiveNounSuffix("sen", "köy")).toBe("köyün");
+  });
+  it("should return the correct possessive noun suffix for 'o'", () => {
+    expect(possessiveNounSuffix("o", "baba")).toBe("babası");
+    expect(possessiveNounSuffix("o", "anne")).toBe("annesi");
+    expect(possessiveNounSuffix("o", "ütü")).toBe("ütüsü");
+    expect(possessiveNounSuffix("o", "anahtar")).toBe("anahtarı");
+    expect(possessiveNounSuffix("o", "ev")).toBe("evi");
+    expect(possessiveNounSuffix("o", "kol")).toBe("kolu");
+    expect(possessiveNounSuffix("o", "köy")).toBe("köyü");
+  });
+  it("should return the correct possessive noun suffix for 'biz'", () => {
+    expect(possessiveNounSuffix("biz", "baba")).toBe("babamız");
+    expect(possessiveNounSuffix("biz", "anne")).toBe("annemiz");
+    expect(possessiveNounSuffix("biz", "manto")).toBe("mantomuz");
+    expect(possessiveNounSuffix("biz", "ütü")).toBe("ütümüz");
+    expect(possessiveNounSuffix("biz", "anahtar")).toBe("anahtarımız");
+    expect(possessiveNounSuffix("biz", "ev")).toBe("evimiz");
+    expect(possessiveNounSuffix("biz", "kol")).toBe("kolumuz");
+    expect(possessiveNounSuffix("biz", "köy")).toBe("köyümüz");
+  });
+  it("should return the correct possessive noun suffix for 'siz'", () => {
+    expect(possessiveNounSuffix("siz", "baba")).toBe("babanız");
+    expect(possessiveNounSuffix("siz", "anne")).toBe("anneniz");
+    expect(possessiveNounSuffix("siz", "manto")).toBe("mantonuz");
+    expect(possessiveNounSuffix("siz", "ütü")).toBe("ütünüz");
+    expect(possessiveNounSuffix("siz", "anahtar")).toBe("anahtarınız");
+    expect(possessiveNounSuffix("siz", "ev")).toBe("eviniz");
+    expect(possessiveNounSuffix("siz", "kol")).toBe("kolunuz");
+    expect(possessiveNounSuffix("siz", "köy")).toBe("köyünüz");
+  });
+  it("should return the correct possessive noun suffix for 'onlar'", () => {
+    expect(possessiveNounSuffix("onlar", "baba")).toBe("babaları");
+    expect(possessiveNounSuffix("onlar", "anne")).toBe("anneleri");
+    expect(possessiveNounSuffix("onlar", "manto")).toBe("mantoları");
+    expect(possessiveNounSuffix("onlar", "ütü")).toBe("ütüleri");
+    expect(possessiveNounSuffix("onlar", "anahtar")).toBe("anahtarları");
+    expect(possessiveNounSuffix("onlar", "ev")).toBe("evleri");
+    expect(possessiveNounSuffix("onlar", "kol")).toBe("kolları");
+    expect(possessiveNounSuffix("onlar", "köy")).toBe("köyleri");
   });
 });
--- src/utils/possessive.ts
+++ src/utils/possessive.ts
@@ -1,3 +1,7 @@
+import { fourWayVowelHarmony, twoWayVowelHarmony } from "./vowelHarmony";
+
+const vowels = "aeıioöuü";
+
 export const possessivePronoun = (pronoun: string) => {
   switch (pronoun.toLowerCase()) {
     case "ben":
@@ -13,4 +17,33 @@ export const possessivePronoun = (pronoun: string) => {
     case "onlar":
       return "onların";
   }
+};
+
+export const possessiveNounSuffix = (pronoun: string, noun: string) => {
+  const lastLetterIsVowel = vowels.includes(noun[noun.length - 1]);
+  const vowel = fourWayVowelHarmony(noun);
+  const pluralVowel = twoWayVowelHarmony(noun);
+  let suffix = "";
+
+  switch (pronoun.toLowerCase()) {
+    case "ben":
+      suffix = lastLetterIsVowel ? "m" : vowel + "m";
+      break;
+    case "sen":
+      suffix = lastLetterIsVowel ? "n" : vowel + "n";
+      break;
+    case "o":
+      suffix = lastLetterIsVowel ? "s" + vowel : vowel;
+      break;
+    case "biz":
+      suffix = lastLetterIsVowel ? "m" + vowel + "z" : vowel + "m" + vowel + "z";
+      break;
+    case "siz":
+      suffix = lastLetterIsVowel ? "n" + vowel + "z" : vowel + "n" + vowel + "z";
+      break;
+    case "onlar":
+      suffix = "l" + pluralVowel + "r" + fourWayVowelHarmony(pluralVowel);
+      break;
+  }
+  return noun + suffix;
 };
