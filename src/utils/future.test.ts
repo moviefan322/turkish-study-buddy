fa5289396b32e5dd8472a1fc5be53a866af544e7
moviefan322 <philman202@yahoo.com>
2024-03-14 15:20:42 -0400

    adds negative future suffix function and test

--- src/utils/future.test.ts
+++ src/utils/future.test.ts
@@ -1,4 +1,4 @@
-import { returnFutureSuffix } from "./future";
+import { returnFutureSuffix, returnFutureSuffixNegative } from "./future";
 
 describe("returnFutureSuffix", () => {
   it("should return the correct future suffix", () => {
@@ -8,5 +8,16 @@ describe("returnFutureSuffix", () => {
     expect(returnFutureSuffix("biz", "gel")).toBe("geleceğiz");
     expect(returnFutureSuffix("siz", "gel")).toBe("geleceksiniz");
     expect(returnFutureSuffix("onlar", "gel")).toBe("gelecekler");
+  });
+});
+
+describe("returnFutureSuffixNegative", () => {
+  it("should return the correct negative future suffix", () => {
+    expect(returnFutureSuffixNegative("ben", "gel")).toBe("gelmeyeceğim");
+    expect(returnFutureSuffixNegative("sen", "gel")).toBe("gelmeyeceksin");
+    expect(returnFutureSuffixNegative("o", "gel")).toBe("gelmeyecek");
+    expect(returnFutureSuffixNegative("biz", "gel")).toBe("gelmeyeceğiz");
+    expect(returnFutureSuffixNegative("siz", "gel")).toBe("gelmeyeceksiniz");
+    expect(returnFutureSuffixNegative("onlar", "gel")).toBe("gelmeyecekler");
   });
 });
--- src/utils/future.ts
+++ src/utils/future.ts
@@ -19,3 +19,22 @@ export const returnFutureSuffix = (pronoun: String, verbStem: string) => {
       return `${verbStem}${vowel}c${vowel}kl${vowel}r`;
   }
 };
+
+export const returnFutureSuffixNegative = (pronoun: String, verbStem: string) => {
+  const vowel = twoWayVowelHarmony(verbStem);
+  const fourWay = fourWayVowelHarmony(vowel);
+  switch (pronoun.toLowerCase()) {
+    case "ben":
+      return `${verbStem}m${vowel}y${vowel}c${vowel}ğ${fourWay}m`;
+    case "sen":
+      return `${verbStem}m${vowel}y${vowel}c${vowel}ks${fourWay}n`;
+    case "o":
+      return `${verbStem}m${vowel}y${vowel}c${vowel}k`;
+    case "biz":
+      return `${verbStem}m${vowel}y${vowel}c${vowel}ğ${fourWay}z`;
+    case "siz":
+      return `${verbStem}m${vowel}y${vowel}c${vowel}ks${fourWay}n${fourWay}z`;
+    case "onlar":
+      return `${verbStem}m${vowel}y${vowel}c${vowel}kl${vowel}r`;
+  }
+};
