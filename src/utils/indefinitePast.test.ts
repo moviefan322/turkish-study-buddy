e6e8e6115ec9fef834d22c2098faaf5c1a6c79bf
moviefan322 <philman202@yahoo.com>
2024-03-24 21:05:04 -0400

    adds function and test for indefinate past interrogative

--- src/utils/indefinitePast.test.ts
+++ src/utils/indefinitePast.test.ts
@@ -1,47 +1,182 @@
-import { returnIndefinitePastSuffix } from "./indefinitePast";
+import {
+  returnIndefinitePastSuffix,
+  indefinitePastAffirmative,
+  indefinitePastNegative,
+  indefinitePastInterrogative,
+} from "./indefinitePast";
 
-describe("indefinitePast", () => {
-  describe("returnIndefinitePastSuffix", () => {
-    it("should return the correct suffix for 'ben' pronoun", () => {
-      expect(returnIndefinitePastSuffix("ben", "ayrıl")).toBe("mışım");
-      expect(returnIndefinitePastSuffix("ben", "git")).toBe("mişim");
-      expect(returnIndefinitePastSuffix("ben", "duy")).toBe("muşum");
-      expect(returnIndefinitePastSuffix("ben", "öksür")).toBe("müşüm");
-    });
+describe("returnIndefinitePastSuffix", () => {
+  it("should return the correct suffix for 'ben' pronoun", () => {
+    expect(returnIndefinitePastSuffix("ben", "ayrıl")).toBe("mışım");
+    expect(returnIndefinitePastSuffix("ben", "git")).toBe("mişim");
+    expect(returnIndefinitePastSuffix("ben", "duy")).toBe("muşum");
+    expect(returnIndefinitePastSuffix("ben", "öksür")).toBe("müşüm");
+  });
 
-    it("should return the correct suffix for 'sen' pronoun", () => {
-      expect(returnIndefinitePastSuffix("sen", "ayrıl")).toBe("mışsın");
-      expect(returnIndefinitePastSuffix("sen", "git")).toBe("mişsin");
-      expect(returnIndefinitePastSuffix("sen", "duy")).toBe("muşsun");
-      expect(returnIndefinitePastSuffix("sen", "öksür")).toBe("müşsün");
-    });
+  it("should return the correct suffix for 'sen' pronoun", () => {
+    expect(returnIndefinitePastSuffix("sen", "ayrıl")).toBe("mışsın");
+    expect(returnIndefinitePastSuffix("sen", "git")).toBe("mişsin");
+    expect(returnIndefinitePastSuffix("sen", "duy")).toBe("muşsun");
+    expect(returnIndefinitePastSuffix("sen", "öksür")).toBe("müşsün");
+  });
 
-    it("should return the correct suffix for 'o' pronoun", () => {
-      expect(returnIndefinitePastSuffix("o", "ayrıl")).toBe("mış");
-      expect(returnIndefinitePastSuffix("o", "git")).toBe("miş");
-      expect(returnIndefinitePastSuffix("o", "duy")).toBe("muş");
-      expect(returnIndefinitePastSuffix("o", "öksür")).toBe("müş");
-    });
+  it("should return the correct suffix for 'o' pronoun", () => {
+    expect(returnIndefinitePastSuffix("o", "ayrıl")).toBe("mış");
+    expect(returnIndefinitePastSuffix("o", "git")).toBe("miş");
+    expect(returnIndefinitePastSuffix("o", "duy")).toBe("muş");
+    expect(returnIndefinitePastSuffix("o", "öksür")).toBe("müş");
+  });
 
-    it("should return the correct suffix for 'biz' pronoun", () => {
-      expect(returnIndefinitePastSuffix("biz", "ayrıl")).toBe("mışız");
-      expect(returnIndefinitePastSuffix("biz", "git")).toBe("mişiz");
-      expect(returnIndefinitePastSuffix("biz", "duy")).toBe("muşuz");
-      expect(returnIndefinitePastSuffix("biz", "öksür")).toBe("müşüz");
-    });
+  it("should return the correct suffix for 'biz' pronoun", () => {
+    expect(returnIndefinitePastSuffix("biz", "ayrıl")).toBe("mışız");
+    expect(returnIndefinitePastSuffix("biz", "git")).toBe("mişiz");
+    expect(returnIndefinitePastSuffix("biz", "duy")).toBe("muşuz");
+    expect(returnIndefinitePastSuffix("biz", "öksür")).toBe("müşüz");
+  });
 
-    it("should return the correct suffix for 'siz' pronoun", () => {
-      expect(returnIndefinitePastSuffix("siz", "ayrıl")).toBe("mışsınız");
-      expect(returnIndefinitePastSuffix("siz", "git")).toBe("mişsiniz");
-      expect(returnIndefinitePastSuffix("siz", "duy")).toBe("muşsunuz");
-      expect(returnIndefinitePastSuffix("siz", "öksür")).toBe("müşsünüz");
-    });
+  it("should return the correct suffix for 'siz' pronoun", () => {
+    expect(returnIndefinitePastSuffix("siz", "ayrıl")).toBe("mışsınız");
+    expect(returnIndefinitePastSuffix("siz", "git")).toBe("mişsiniz");
+    expect(returnIndefinitePastSuffix("siz", "duy")).toBe("muşsunuz");
+    expect(returnIndefinitePastSuffix("siz", "öksür")).toBe("müşsünüz");
+  });
 
-    it("should return the correct suffix for 'onlar' pronoun", () => {
-      expect(returnIndefinitePastSuffix("onlar", "ayrıl")).toBe("mışlar");
-      expect(returnIndefinitePastSuffix("onlar", "git")).toBe("mişler");
-      expect(returnIndefinitePastSuffix("onlar", "duy")).toBe("muşlar");
-      expect(returnIndefinitePastSuffix("onlar", "öksür")).toBe("müşler");
-    });
+  it("should return the correct suffix for 'onlar' pronoun", () => {
+    expect(returnIndefinitePastSuffix("onlar", "ayrıl")).toBe("mışlar");
+    expect(returnIndefinitePastSuffix("onlar", "git")).toBe("mişler");
+    expect(returnIndefinitePastSuffix("onlar", "duy")).toBe("muşlar");
+    expect(returnIndefinitePastSuffix("onlar", "öksür")).toBe("müşler");
+  });
+});
+
+describe("indefinitePastAffirmative", () => {
+  it("should return the correct indefinite past affirmative for 'ben' pronoun", () => {
+    expect(indefinitePastAffirmative("ben", "ayrıl")).toBe("ayrılmışım");
+    expect(indefinitePastAffirmative("ben", "git")).toBe("gitmişim");
+    expect(indefinitePastAffirmative("ben", "duy")).toBe("duymuşum");
+    expect(indefinitePastAffirmative("ben", "öksür")).toBe("öksürmüşüm");
+  });
+
+  it("should return the correct indefinite past affirmative for 'sen' pronoun", () => {
+    expect(indefinitePastAffirmative("sen", "ayrıl")).toBe("ayrılmışsın");
+    expect(indefinitePastAffirmative("sen", "git")).toBe("gitmişsin");
+    expect(indefinitePastAffirmative("sen", "duy")).toBe("duymuşsun");
+    expect(indefinitePastAffirmative("sen", "öksür")).toBe("öksürmüşsün");
+  });
+
+  it("should return the correct indefinite past affirmative for 'o' pronoun", () => {
+    expect(indefinitePastAffirmative("o", "ayrıl")).toBe("ayrılmış");
+    expect(indefinitePastAffirmative("o", "git")).toBe("gitmiş");
+    expect(indefinitePastAffirmative("o", "duy")).toBe("duymuş");
+    expect(indefinitePastAffirmative("o", "öksür")).toBe("öksürmüş");
+  });
+
+  it("should return the correct indefinite past affirmative for 'biz' pronoun", () => {
+    expect(indefinitePastAffirmative("biz", "ayrıl")).toBe("ayrılmışız");
+    expect(indefinitePastAffirmative("biz", "git")).toBe("gitmişiz");
+    expect(indefinitePastAffirmative("biz", "duy")).toBe("duymuşuz");
+    expect(indefinitePastAffirmative("biz", "öksür")).toBe("öksürmüşüz");
+  });
+
+  it("should return the correct indefinite past affirmative for 'siz' pronoun", () => {
+    expect(indefinitePastAffirmative("siz", "ayrıl")).toBe("ayrılmışsınız");
+    expect(indefinitePastAffirmative("siz", "git")).toBe("gitmişsiniz");
+    expect(indefinitePastAffirmative("siz", "duy")).toBe("duymuşsunuz");
+    expect(indefinitePastAffirmative("siz", "öksür")).toBe("öksürmüşsünüz");
+  });
+
+  it("should return the correct indefinite past affirmative for 'onlar' pronoun", () => {
+    expect(indefinitePastAffirmative("onlar", "ayrıl")).toBe("ayrılmışlar");
+    expect(indefinitePastAffirmative("onlar", "git")).toBe("gitmişler");
+    expect(indefinitePastAffirmative("onlar", "duy")).toBe("duymuşlar");
+    expect(indefinitePastAffirmative("onlar", "öksür")).toBe("öksürmüşler");
+  });
+});
+
+describe("indefinitePastNegative", () => {
+  it("should return the correct indefinite past negative for 'ben' pronoun", () => {
+    expect(indefinitePastNegative("ben", "ayrıl")).toBe("ayrılmamışım");
+    expect(indefinitePastNegative("ben", "git")).toBe("gitmemişim");
+    expect(indefinitePastNegative("ben", "duy")).toBe("duymamışım");
+    expect(indefinitePastNegative("ben", "öksür")).toBe("öksürmemişim");
+  });
+
+  it("should return the correct indefinite past negative for 'sen' pronoun", () => {
+    expect(indefinitePastNegative("sen", "ayrıl")).toBe("ayrılmamışsın");
+    expect(indefinitePastNegative("sen", "git")).toBe("gitmemişsin");
+    expect(indefinitePastNegative("sen", "duy")).toBe("duymamışsın");
+    expect(indefinitePastNegative("sen", "öksür")).toBe("öksürmemişsin");
+  });
+
+  it("should return the correct indefinite past negative for 'o' pronoun", () => {
+    expect(indefinitePastNegative("o", "ayrıl")).toBe("ayrılmamış");
+    expect(indefinitePastNegative("o", "git")).toBe("gitmemiş");
+    expect(indefinitePastNegative("o", "duy")).toBe("duymamış");
+    expect(indefinitePastNegative("o", "öksür")).toBe("öksürmemiş");
+  });
+
+  it("should return the correct indefinite past negative for 'biz' pronoun", () => {
+    expect(indefinitePastNegative("biz", "ayrıl")).toBe("ayrılmamışız");
+    expect(indefinitePastNegative("biz", "git")).toBe("gitmemişiz");
+    expect(indefinitePastNegative("biz", "duy")).toBe("duymamışız");
+    expect(indefinitePastNegative("biz", "öksür")).toBe("öksürmemişiz");
+  });
+
+  it("should return the correct indefinite past negative for 'siz' pronoun", () => {
+    expect(indefinitePastNegative("siz", "ayrıl")).toBe("ayrılmamışsınız");
+    expect(indefinitePastNegative("siz", "git")).toBe("gitmemişsiniz");
+    expect(indefinitePastNegative("siz", "duy")).toBe("duymamışsınız");
+    expect(indefinitePastNegative("siz", "öksür")).toBe("öksürmemişsiniz");
+  });
+
+  it("should return the correct indefinite past negative for 'onlar' pronoun", () => {
+    expect(indefinitePastNegative("onlar", "ayrıl")).toBe("ayrılmamışlar");
+    expect(indefinitePastNegative("onlar", "git")).toBe("gitmemişler");
+    expect(indefinitePastNegative("onlar", "duy")).toBe("duymamışlar");
+    expect(indefinitePastNegative("onlar", "öksür")).toBe("öksürmemişler");
+  });
+});
+
+describe("indefinitePastInterrogative", () => {
+  it("should return the correct indefinite past interrogative for 'ben' pronoun", () => {
+    expect(indefinitePastInterrogative("ben", "ayrıl")).toBe("ayrılmış mıyım");
+    expect(indefinitePastInterrogative("ben", "git")).toBe("gitmiş miyim");
+    expect(indefinitePastInterrogative("ben", "duy")).toBe("duymuş muyum");
+    expect(indefinitePastInterrogative("ben", "öksür")).toBe("öksürmüş müyüm");
+  });
+
+  it("should return the correct indefinite past interrogative for 'sen' pronoun", () => {
+    expect(indefinitePastInterrogative("sen", "ayrıl")).toBe("ayrılmış mısın");
+    expect(indefinitePastInterrogative("sen", "git")).toBe("gitmiş misin");
+    expect(indefinitePastInterrogative("sen", "duy")).toBe("duymuş musun");
+    expect(indefinitePastInterrogative("sen", "öksür")).toBe("öksürmüş müsün");
+  });
+
+  it("should return the correct indefinite past interrogative for 'o' pronoun", () => {
+    expect(indefinitePastInterrogative("o", "ayrıl")).toBe("ayrılmış mı");
+    expect(indefinitePastInterrogative("o", "git")).toBe("gitmiş mi");
+    expect(indefinitePastInterrogative("o", "duy")).toBe("duymuş mu");
+    expect(indefinitePastInterrogative("o", "öksür")).toBe("öksürmüş mü");
+  });
+
+  it("should return the correct indefinite past interrogative for 'biz' pronoun", () => {
+    expect(indefinitePastInterrogative("biz", "ayrıl")).toBe("ayrılmış mıyız");
+    expect(indefinitePastInterrogative("biz", "git")).toBe("gitmiş miyiz");
+    expect(indefinitePastInterrogative("biz", "duy")).toBe("duymuş muyuz");
+    expect(indefinitePastInterrogative("biz", "öksür")).toBe("öksürmüş müyüz");
+  });
+
+  it("should return the correct indefinite past interrogative for 'siz' pronoun", () => {
+    expect(indefinitePastInterrogative("siz", "ayrıl")).toBe("ayrılmış mısınız");
+    expect(indefinitePastInterrogative("siz", "git")).toBe("gitmiş misiniz");
+    expect(indefinitePastInterrogative("siz", "duy")).toBe("duymuş musunuz");
+    expect(indefinitePastInterrogative("siz", "öksür")).toBe("öksürmüş müsünüz");
+  });
+
+  it("should return the correct indefinite past interrogative for 'onlar' pronoun", () => {
+    expect(indefinitePastInterrogative("onlar", "ayrıl")).toBe("ayrılmışlar mı");
+    expect(indefinitePastInterrogative("onlar", "git")).toBe("gitmişler mi");
+    expect(indefinitePastInterrogative("onlar", "duy")).toBe("duymuşlar mı");
+    expect(indefinitePastInterrogative("onlar", "öksür")).toBe("öksürmüşler mi");
   });
 });
--- src/utils/indefinitePast.ts
+++ src/utils/indefinitePast.ts
@@ -1,5 +1,10 @@
 import { fourWayVowelHarmony, twoWayVowelHarmony } from "./vowelHarmony";
 
+export const returnMişForm = (verb: string) => {
+  const vowel = fourWayVowelHarmony(verb);
+  return `m${vowel}ş`;
+};
+
 export const returnIndefinitePastSuffix = (pronoun: string, verb: string) => {
   const vowel = fourWayVowelHarmony(verb);
   switch (pronoun.toLowerCase()) {
@@ -15,5 +20,38 @@ export const returnIndefinitePastSuffix = (pronoun: string, verb: string) => {
       return `m${vowel}şs${vowel}n${vowel}z`;
     case "onlar":
       return `m${vowel}şl${twoWayVowelHarmony(vowel)}r`;
+  }
+};
+
+export const indefinitePastAffirmative = (pronoun: string, verb: string) => {
+  return `${verb}${returnIndefinitePastSuffix(pronoun, verb)}`;
+};
+
+export const indefinitePastNegative = (pronoun: string, verb: string) => {
+  const negatedVerbRoot = `${verb}m${twoWayVowelHarmony(verb)}`;
+  const vowel = fourWayVowelHarmony(negatedVerbRoot);
+
+  return `${negatedVerbRoot}${returnIndefinitePastSuffix(pronoun, negatedVerbRoot)}`;
+};
+
+export const indefinitePastInterrogative = (pronoun: string, verb: string) => {
+  const verbPlusMiş = `${verb}${returnMişForm(verb)}`;
+  const vowel = fourWayVowelHarmony(verbPlusMiş);
+
+  switch (pronoun.toLowerCase()) {
+    case "ben":
+      return `${verbPlusMiş} m${vowel}y${vowel}m`;
+    case "sen":
+      return `${verbPlusMiş} m${vowel}s${vowel}n`;
+    case "o":
+      return `${verbPlusMiş} m${vowel}`;
+    case "biz":
+      return `${verbPlusMiş} m${vowel}y${vowel}z`;
+    case "siz":
+      return `${verbPlusMiş} m${vowel}s${vowel}n${vowel}z`;
+    case "onlar":
+      return `${verbPlusMiş}l${twoWayVowelHarmony(verbPlusMiş)}r m${fourWayVowelHarmony(
+        twoWayVowelHarmony(verbPlusMiş)
+      )}`;
   }
 };
