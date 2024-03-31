be4e672874c2f6c67f521d5aab2ddaa03325c0d3
moviefan322 <philman202@yahoo.com>
2024-03-30 22:45:33 -0400

    adds duyulan gecmis zaman to menu

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -32,6 +32,12 @@ const Menu = () => {
         </div>
         <div className="mb-3">
           {" "}
+          <Link href="/indefinitePast">
+            <button className="btn btn-lg btn-dark text-light w-100 px-5 fs-1">Duyulan Geçmiş Zaman</button>
+          </Link>
+        </div>
+        <div className="mb-3">
+          {" "}
           <Link href="/flashcard">
             <button className="btn btn-lg btn-dark text-light w-100 px-5 fs-1">Şimşek Kartlar</button>
           </Link>
